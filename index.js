9378afafc5a873a81af346d05fbfc4172589d80f
Ibene Liz Mathew <ibeneliz@gmail.com>
2023-09-03 18:23:30 +0530

    Error handling

--- index.js
+++ index.js
@@ -19,53 +19,57 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // this block will run when the client connects
 io.on('connection', socket => {
-    socket.on('joinRoom', ({ username, room }) => {
-      const user = newUser(socket.id, username, room);
-  
-      socket.join(user.room);
-  
-      // General welcome
-      socket.emit('message', formatMessage("Airtribe", 'Messages are limited to this room! '));
-  
-      // Broadcast everytime users connects
-      socket.broadcast
-        .to(user.room)
-        .emit(
-          'message',
-          formatMessage("Airtribe", `${user.username} has joined the room`)
-        );
-  
-      // Current active users and room name
-      io.to(user.room).emit('roomUsers', {
-        room: user.room,
-        users: getIndividualRoomUsers(user.room)
-      });
-    });
-  
-    // Listen for client message
-    socket.on('chatMessage', msg => {
-      const user = getActiveUser(socket.id);
-  
-      io.to(user.room).emit('message', formatMessage(user.username, msg));
-    });
-  
-    // Runs when client disconnects
-    socket.on('disconnect', () => {
-      const user = exitRoom(socket.id);
-  
-      if (user) {
-        io.to(user.room).emit(
-          'message',
-          formatMessage("Airtribe", `${user.username} has left the room`)
-        );
-  
+    try{
+        socket.on('joinRoom', ({ username, room }) => {
+        const user = newUser(socket.id, username, room);
+    
+        socket.join(user.room);
+    
+        // General welcome
+        socket.emit('message', formatMessage("Airtribe", 'Messages are limited to this room! '));
+    
+        // Broadcast everytime users connects
+        socket.broadcast
+            .to(user.room)
+            .emit(
+            'message',
+            formatMessage("Airtribe", `${user.username} has joined the room`)
+            );
+    
         // Current active users and room name
         io.to(user.room).emit('roomUsers', {
-          room: user.room,
-          users: getIndividualRoomUsers(user.room)
+            room: user.room,
+            users: getIndividualRoomUsers(user.room)
         });
-      }
-    });
+        });
+    
+        // Listen for client message
+        socket.on('chatMessage', msg => {
+        const user = getActiveUser(socket.id);
+    
+        io.to(user.room).emit('message', formatMessage(user.username, msg));
+        });
+    
+        // Runs when client disconnects
+        socket.on('disconnect', () => {
+        const user = exitRoom(socket.id);
+    
+        if (user) {
+            io.to(user.room).emit(
+            'message',
+            formatMessage("Airtribe", `${user.username} has left the room`)
+            );
+    
+            // Current active users and room name
+            io.to(user.room).emit('roomUsers', {
+            room: user.room,
+            users: getIndividualRoomUsers(user.room)
+            });
+        }
+        });
+    }catch{
+        console.log("Error occured in connecting", error);
+    }
   });
 
 
--- public/js/main.js
+++ public/js/main.js
@@ -26,40 +26,48 @@ socket.on('message', (message) => {
 
 // Message submit
 chatForm.addEventListener('submit', (e) => {
-  e.preventDefault();
+  try{
+    e.preventDefault();
 
-  // Get message text
-  let msg = e.target.elements.msg.value;
-  msg = msg.trim();
+    // Get message text
+    let msg = e.target.elements.msg.value;
+    msg = msg.trim();
 
-  if (!msg) {
-    return false;
+    if (!msg) {
+      return false;
+    }
+
+    // Emit message to server
+    socket.emit('chatMessage', msg);
+
+    // Clear input
+    e.target.elements.msg.value = '';
+    e.target.elements.msg.focus();
+  }catch(error){
+    console.log("Error in sending chat message from client to server", error);
   }
-
-  // Emit message to server
-  socket.emit('chatMessage', msg);
-
-  // Clear input
-  e.target.elements.msg.value = '';
-  e.target.elements.msg.focus();
 });
 
 function outputMessage(message) {
-  const div = document.createElement('div');
-  div.classList.add('message');
+  try{
+    const div = document.createElement('div');
+    div.classList.add('message');
 
-  const p = document.createElement('p');
-  p.classList.add('meta');
-  p.innerText = message.username;
-  p.innerHTML += `<span>${message.time}</span>`;
-  div.appendChild(p);
+    const p = document.createElement('p');
+    p.classList.add('meta');
+    p.innerText = message.username;
+    p.innerHTML += `<span>${message.time}</span>`;
+    div.appendChild(p);
 
-  const para = document.createElement('p');
-  para.classList.add('text');
-  para.innerText = message.text;
-  div.appendChild(para);
+    const para = document.createElement('p');
+    para.classList.add('text');
+    para.innerText = message.text;
+    div.appendChild(para);
 
-  document.querySelector('.chat-messages').appendChild(div);
+    document.querySelector('.chat-messages').appendChild(div);
+  }catch(error){
+    console.log("Error in displaying chat message in chat room", error);
+  }
 }
 
 function outputRoomName(room) {
