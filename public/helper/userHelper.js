0d41387968e40fe8f9fc80481a9c0b46bf19469a
Ibene Liz Mathew <ibeneliz@gmail.com>
2023-09-04 00:37:43 +0530

    Error handling and input validation

--- public/helper/userHelper.js
+++ public/helper/userHelper.js
@@ -2,35 +2,43 @@ const users = [];
 
 // Join user to chat
 function newUser(id, username, room) {
-  const user = { id, username, room };
+    const user = { id, username, room };
 
-  users.push(user);
+    users.push(user);
 
-  return user;
+    return user;
 }
 
 // Get current user
 function getActiveUser(id) {
-  return users.find(user => user.id === id);
+    try {
+        return users.find(user => user.id === id);
+    } catch (error) {
+        console.log("Error occureed in finding the active user", error);
+    }
 }
 
 // User leaves chat
 function exitRoom(id) {
-  const index = users.findIndex(user => user.id === id);
+    const index = users.findIndex(user => user.id === id);
 
-  if (index !== -1) {
-    return users.splice(index, 1)[0];
-  }
+    if (index !== -1) {
+        return users.splice(index, 1)[0];
+    }
 }
 
 // Get room users
 function getIndividualRoomUsers(room) {
-  return users.filter(user => user.room === room);
+    try {
+        return users.filter(user => user.room === room);
+    } catch (error) {
+        console.log("Error occureed in finding users in the room.", error);
+    }
 }
 
 module.exports = {
-  newUser,
-  getActiveUser,
-  exitRoom,
-  getIndividualRoomUsers
+    newUser,
+    getActiveUser,
+    exitRoom,
+    getIndividualRoomUsers
 };
